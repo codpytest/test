c6c9d2aec8e181b19010de219bf79b3bc1f0bcec
codpy2020 <67156845+codpy2020@users.noreply.github.com>
2022-12-13 17:00:43 +0100

    ubuntu

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@
 #
 # Copyright (C) 
 
-from setuptools import setup, Extension, Command
-from setuptools import setup, find_packages
 from __future__ import annotations
-from distutils.core import setup
-from distutils import sysconfig
-import os,sys
-from shutil import copy
-__version__ = '0.1.7'
+from setuptools import setup, Extension, Command
+__version__ = '0.0.1'
 
 
 
