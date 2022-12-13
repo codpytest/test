2e5e45a3b1bbbd566c501b13b2290623df1af58a
codpy2020 <67156845+codpy2020@users.noreply.github.com>
2022-12-13 16:12:06 +0100

    windows

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ import os,sys
 from shutil import copy
 __version__ = '0.1.7'
 
-DISTNAME = 'codpy'
-DESCRIPTION = 'An RKHS based module for machine learning and data mining'
-MAINTAINER = 'jean-marc mercier'
-MAINTAINER_EMAIL = 'jeanmarc.mercier@gmail.com'
-URL = 'https://github.com/johnlem/codpy_alpha'
+DISTNAME = 'test'
+DESCRIPTION = 'Test'
+MAINTAINER = 'BB'
+MAINTAINER_EMAIL = 'bg'
+URL = 'www.appleworld.com'
 #DOWNLOAD_URL = 'https://github.com/johnlem/codpy_alpha'
 LICENSE = 'new BSD'
 PROJECT_URLS = {
@@ -36,7 +36,7 @@ PROJECT_URLS = {
 #extra_files = package_files(codpy_path)
 long_description = open("README.md","r").read()
 
-#print("find_packages():",find_packages(),)
+print("find_packages():",find_packages(),)
 
 
 
@@ -46,7 +46,7 @@ include_dirs = []
 
 
 # Build extension with previously Cython generated source.
-ext_modules.append(Extension("printf", ["codpy/printf.c"]))
+# ext_modules.append(Extension("printf", ["codpy/printf.c"]))
 
 
 
@@ -60,7 +60,7 @@ setup(
     description=DESCRIPTION,
     license=LICENSE,
     url=URL,
-    packages=['codpy'],
+    packages= find_packages(),
     include_package_data=True,
     #package_data={'': extra_files},
     classifiers=[
@@ -74,8 +74,7 @@ setup(
     ],
     #install_requires=['SobolSequence==0.2.1','google-api-python-client==2.63.0','oauth2client==4.1.3','pybind11==2.10.0','pandas==1.5.0','tk','numpy==1.22.0','matplotlib==3.6.0','mkl','scikit-learn==1.1.3','scipy==1.9.2','tensorflow==2.10.0','seaborn==0.12.0', 'scikit-image==0.19.3','tensorflow-datasets==4.6.0','torch==1.12.1','xgboost==1.6.2','jupyter==1.0.0','quantlib==1.27','xlrd==2.0.1','pydicom==2.3.0','yfinance==0.1.74','xarray==2022.9.0','kaggle==1.5.12','pandera','statsmodels'],
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    ext_modules=ext_modules
+    long_description_content_type='text/markdown'
   #  extras_require={
   #  'win32': 'pywin32'
   #}
