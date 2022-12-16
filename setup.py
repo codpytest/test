4c77f91ef72a4eeed4a5a1b644db32d87a446068
codpy2020 <67156845+codpy2020@users.noreply.github.com>
2022-12-16 09:58:38 +0100

    update

--- setup.py
+++ setup.py
@@ -7,4 +7,21 @@ __version__ = '0.0.1'
 
 
 
-setup(ext_modules=[Extension(name="printf", sources=["src/printf.cpp"])])
+printf = Extension(
+    'printf',
+    sources=["src/printf.cpp"],
+    # include_dirs=[pybind11.get_include()],
+)
+
+if __name__ == "__main__":
+    setup(
+        name = 'test',
+        version=__version__,
+        author="test",
+        author_email="",
+        url="",
+        description="test",
+        long_description="",
+        ext_modules=[printf],
+        python_requires=">=3.7"
+    )
